abb0eaf47a8e947f93fe809b99adac117a553bf4
Zahrah Al-samach <zal-samach@mail.greenriver.edu>
2021-04-15 12:36:01 -0400

    title

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    // Zahrah Al-samach
+    // 04/15/21
+    // https://github.com/zalsamach/pp2.git
+    // Pair Program 2
+
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 2</title>
+</head>
+<body>
+    <?php
+        echo "<p>PHP Array Practice</p>";
+
+    ?>
+
+</body>
+</html>
